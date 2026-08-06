d3bd926542ae4412559898ba6d9b17e90e4b6053
Akshat_Singh <152872166+akshatharshit@users.noreply.github.com>
2026-08-07 03:39:11 +0530

    Time: 0 ms (100%), Space: 43.2 MB (82.13%) - LeetHub

---
+++ 0998-maximum-binary-tree-ii/0998-maximum-binary-tree-ii.java
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode insertIntoMaxTree(TreeNode root, int val) {
+        if(root==null)return new TreeNode(val);
+        if(root.val<val){
+            TreeNode nr=new TreeNode(val);
+            nr.left=root;
+            return nr;
+        }
+        root.right=insertIntoMaxTree(root.right,val);
+        return root;
+    }
+}
