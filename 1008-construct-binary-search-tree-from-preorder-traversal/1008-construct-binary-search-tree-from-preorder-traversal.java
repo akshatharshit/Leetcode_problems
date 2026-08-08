fe2a2e7387e7c878d37266c54da61b3bc9c29fd3
Akshat_Singh <152872166+akshatharshit@users.noreply.github.com>
2026-08-08 11:46:11 +0530

    Time: 0 ms (100%), Space: 43.6 MB (41.4%) - LeetHub

--- 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.java
+++ 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.java
@@ -14,18 +14,28 @@
  * }
  */
 class Solution {
-    public TreeNode insert(TreeNode r,int v){
-        if(r==null)return new TreeNode(v);
-        if(r.val>v){
-            r.left=insert(r.left,v);
-        }else if(r.val<v){
-            r.right=insert(r.right,v);
-        }
+    // public TreeNode insert(TreeNode r,int v){
+    //     if(r==null)return new TreeNode(v);
+    //     if(r.val>v){
+    //         r.left=insert(r.left,v);
+    //     }else if(r.val<v){
+    //         r.right=insert(r.right,v);
+    //     }
+    //     return r;
+    // }
+    int i=0;
+    public TreeNode sol(int a[],int b){
+        if(i==a.length || a[i]>b)return null;
+        TreeNode r=new TreeNode(a[i++]);
+        r.left=sol(a,r.val);
+        r.right=sol(a,b);
         return r;
     }
     public TreeNode bstFromPreorder(int[] p) {
-        TreeNode r=null;
-        for(int i: p)r=insert(r,i);
-        return r;
+        // TreeNode r=null;
+        // for(int i: p)r=insert(r,i);
+        // return r;
+
+        return sol(p,Integer.MAX_VALUE);
     } 
 }
